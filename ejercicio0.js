d51e8a9337127cc03055df6221c9a2fbeeeb9881
Pedro Gonzales <pedro281195@gmail.com>
2020-10-10 08:30:03 -0500

    Ejercicio 0

---
+++ ejercicio0.js
@@ -0,0 +1,6 @@
+//Ejercicio 0
+//###############################################################################
+let numbers = new Array(21,200,15,5,1,13);
+let minNumber = Math.min(...numbers);
+console.info('Ejercicio 0');
+console.log('Número menor', minNumber);
