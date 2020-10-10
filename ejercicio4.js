b9bfdd25117db779ee2c72e1dcacc249d6237a24
Pedro Gonzales <pedro281195@gmail.com>
2020-10-10 08:55:25 -0500

    Ejercicio 4

---
+++ ejercicio4.js
@@ -0,0 +1,8 @@
+//Ejercicio 4
+//###############################################################################
+let months =new Array('Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Setiembre', 'Octubre', 'Noviembre', 'Diciembre');
+
+console.info('Ejercicio 4');
+for(const month of months) {
+    console.log(month)
+}
