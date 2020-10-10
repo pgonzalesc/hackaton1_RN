228159430066eb4ed1f887d7bcb0bd435682ab0e
Pedro Gonzales <pedro281195@gmail.com>
2020-10-10 10:31:54 -0500

    Corrigiendo Ejercicio 6

--- ejercicio6.js
+++ ejercicio6.js
@@ -2,10 +2,14 @@
 //###############################################################################
 console.info('Ejercicio 6');
 
+function Contact(name, phone){
+    this.name = name;
+    this.phone = phone;
+}
 
-let contact1 = new Array('Pedro', '123456789');
-let contact2 = new Array('Maria', '987654321');
-let contact3 = new Array('Pepe', '987654322');
+let contact1 = new Contact('Pedro', '123456789');
+let contact2 = new Contact('Maria', '987654321');
+let contact3 = new Contact('Pepe', '987654322');
 
 let listContact = [];
 listContact.push(contact1);
@@ -13,12 +17,13 @@ listContact.push(contact2);
 listContact.push(contact3);
 
 function call(phone, listContact){
-    for(let key=0; key < listContact.length; key++){
-        if(phone == listContact[key][1])
-            return console.log(`${listContact[key][0]} está llamando`);
+    for(let key in listContact) {
+        if(phone === listContact[key].phone)
+            return console.log(`${listContact[key].name} está llamando`);
         else
             return console.log('Número desconocido');
     }
 }
 
-call('123456780', listContact);
+call('123456789', listContact);
+
